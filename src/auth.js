a97c2414850266474d614962c0c5a0b5127c5122
Bronston Ashford <z5146619@ad.unsw.edu.au>
2022-09-20 11:25:10 +1000

    fixed brackets

--- src/auth.js
+++ src/auth.js
@@ -1,24 +1,15 @@
 
 
 // Stub-function for authenticating user login
-function authLoginV1(email, password) 
-{
-  return 
-  {
+function authLoginV1(email, password) {
+  return {
     authUserId: 1,
   }
 }
 
-
-
-
-
-
 //Stub-function for registering user.
-function authRegisterV1(email, password, nameFirst, nameLast)
-{
-  return 
-  {
+function authRegisterV1(email, password, nameFirst, nameLast){
+  return {
     authUserId: 1,
   }
 }
