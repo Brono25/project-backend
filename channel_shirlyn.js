9b06d6e0219cb2fec054d65208e34fc7a9bda382
Shirlyn Alwis <z5417225@ad.unsw.edu.au>
2022-09-21 16:13:06 +1000

    Created two functions

---
+++ channel_shirlyn.js
@@ -0,0 +1,18 @@
+// Stub funtion for creating user channels.
+function channelsCreateV1(authUserId, name, isPublic) {
+    return {
+        channelId: 1,
+    }
+}
+
+// Stub function for listing the created channels.
+function channelsListV1(authUserId) {
+    return {
+        channels: [
+            {
+                channelId: 1, 
+                name: 'My Channel',
+            }
+        ],
+    }
+}
