7b58f33434587ebe35c3560bc96f899a5e942155
Shirlyn Alwis <z5417225@ad.unsw.edu.au>
2022-09-21 22:20:52 +1000

    fixed some indentation errors

--- src/channels.js
+++ src/channels.js
@@ -14,19 +14,19 @@ function channelsListAllV1 (authUserId) {
 
 // Stub funtion for creating user channels.
 function channelsCreateV1(authUserId, name, isPublic) {
-    return {
-        channelId: 1,
+  return {
+      channelId: 1,
     }
 }
 
 // Stub function for listing the created channels.
 function channelsListV1(authUserId) {
-    return {
-        channels: [
-            {
-                channelId: 1, 
-                name: 'My Channel',
-            }
-        ],
-    }
+  return {
+    channels: [
+        {
+            channelId: 1, 
+            name: 'My Channel',
+        }
+    ],
+  }
 }
