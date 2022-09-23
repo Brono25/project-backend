fde05e813b21a96d4e60fe9e4dba71b6d8e9d24c
Shirlyn Alwis <z5417225@ad.unsw.edu.au>
2022-09-23 18:59:34 +1000

    deleted channelsListV1

--- src/channels.js
+++ src/channels.js
@@ -17,15 +17,5 @@ function channelsCreateV1(authUserId, name, isPublic) {
   }
 }
 
-// Stub function for listing the created channels.
-function channelsListV1(authUserId) {
-  return {
-    channels: [
-      {
-        channelId: 1, 
-        name: 'My Channel',
-      }
-    ],
-  }
-}
+
 
