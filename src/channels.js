52b0cd7dd156fdda945c57ed88aff21299d840e2
Shirlyn Alwis <z5417225@ad.unsw.edu.au>
2022-09-23 14:25:24 +1000

    Added channelsListV1

--- src/channels.js
+++ src/channels.js
@@ -17,4 +17,15 @@ function channelsCreateV1(authUserId, name, isPublic) {
   }
 }
 
+// Stub function for listing the created channels.
+function channelsListV1(authUserId) {
+  return {
+    channels: [
+      {
+        channelId: 1, 
+        name: 'My Channel',
+      }
+    ],
+  }
+}
 
