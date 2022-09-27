49f91d2ec7851277bac0086e8414dc4fd19a24f1
Brono25 <brono_25@hotmail.com>
2022-09-28 02:03:34 +1000

    added basic test for clearV1

---
+++ src/other.test.js
@@ -0,0 +1,17 @@
+import {
+  authRegisterV1,
+} from './auth';
+
+import {clearV1} from './other';
+
+
+
+describe('clearV1()', () => {
+
+    test('Adding duplicate data after clearV1 doesnt error', () => {
+      const args = ['test@gmail.com', '123456', 'firstName', 'lastName'];
+      expect(authRegisterV1(...args)).toStrictEqual({authUserId: expect.any(Number)});
+      clearV1();
+      expect(authRegisterV1(...args)).toStrictEqual({authUserId: expect.any(Number)});
+    });
+}); 
