c9d873f9cdb67801742e65cee3d6956296cd1e13
Brono25 <brono_25@hotmail.com>
2022-09-28 12:58:45 +1000

    channel create function adding

--- src/channels.js
+++ src/channels.js
@@ -1,3 +1,39 @@
+
+import {
+  setData,
+  getData,
+} from './dataStore.js';
+
+import {
+  isValidAuthUserId,
+} from './other'
+
+//------------------Channels Helper functions------------------
+
+
+
+//------------------Channels Main functions------------------
+
+// Stub funtion for creating user channels.
+function channelsCreateV1(authUserId, name, isPublic) {
+
+  const maxChars = 20;
+  const minChars = 1;
+  if(name.length > maxChars || name.length < minChars) {
+    return {error: 'Channels name must be between 1-20 characters (inclusive)'};
+  }
+  if(!isValidAuthUserId(authUserId)) {
+    return {error: 'Invalid User ID'};
+  }
+
+
+  return {
+    channelId: 1,
+  }
+}
+
+
+
 // Stub-function for listing all channels
 function channelsListAllV1(authUserId) {
   return {
@@ -10,12 +46,7 @@ function channelsListAllV1(authUserId) {
   }
 }
 
-// Stub funtion for creating user channels.
-function channelsCreateV1(authUserId, name, isPublic) {
-  return {
-    channelId: 1,
-  }
-}
+
 
 // Stub function for listing the created channels.
 function channelsListV1(authUserId) {
