772989b98fd115a93b80d5503e4c2e48123bde79
Brono25 <brono_25@hotmail.com>
2022-10-20 18:21:16 +1100

    feature: error handling for dms

--- src/dm.ts
+++ src/dm.ts
@@ -7,8 +7,17 @@ import {
   isValidAuthUserId,
   getTokenOwnersUid,
   isActiveToken,
+  getHandleStrfromUid,
 } from './other';
 
+// ////////////////////////////////////////////////////// //
+//                      dmCreateV1                        //
+// ////////////////////////////////////////////////////// //
+/**
+ *
+ * @param {string, number[]}
+ * @returns {number}
+ */
 export function dmCreateV1(token: string, uIds: number[]): DmCreateReturn {
   if (uIds.find(a => isValidAuthUserId(a) === false)) {
     return { error: 'Invalid User ID' };
@@ -24,6 +33,24 @@ export function dmCreateV1(token: string, uIds: number[]): DmCreateReturn {
   if (!isActiveToken(token)) {
     return { error: 'Invalid Token' };
   }
+  uIds.unshift(creatorId);
+  generateDmName(uIds);
 
   return { dmId: -1 };
+}
+
+/**
+ * @param {number[]}
+ * @returns {string}
+ */
+function generateDmName(uIds: number[]) {
+  const handleStrList: string[] = [];
+  for (const uId of uIds) {
+    const handleStr = getHandleStrfromUid(uId);
+    handleStrList.push(handleStr);
+  }
+  const name = handleStrList.sort(
+    (a, b) => a.toLowerCase().localeCompare(b.toLowerCase())
+  ).join(', ');
+  return name;
 }
