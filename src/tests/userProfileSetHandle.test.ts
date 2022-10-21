5151231096a11838e753c1835e80b023ce6e50ad
Lamiya <z5356304@ad.unsw.edu.au>
2022-10-21 23:28:14 +1100

    added tests for UPsethandle

---
+++ src/tests/userProfileSetHandle.test.ts
@@ -0,0 +1,82 @@
+import * as h from './test.helper';
+
+// Setup: Create 3 users.
+let authUser0: any;
+let authUser2: any;
+let token0: string;
+let token2: string;
+let handleStr0: string;
+let handleStr2: string;
+
+beforeEach(() => {
+    authUser0 = h.postRequest(h.REGISTER_URL, {
+        email: h.email0,
+        password: h.password0,
+        nameFirst: h.firstName0,
+        nameLast: h.lastName0,
+    });
+
+    authUser2 = h.postRequest(h.REGISTER_URL, {
+        email: h.email2,
+        password: h.password2,
+        nameFirst: h.firstName2,
+        nameLast: h.lastName2,
+      });
+    
+    token0 = authUser0.token;
+    token2 = authUser2.token;
+    handleStr0 = authUser0.handleStr;
+    handleStr2 = authUser0.handleStr;
+});
+// Tear down
+afterEach(() => {
+  h.deleteRequest(h.CLEAR_URL, {});
+});
+
+// ------------------Error Testing------------------//
+
+describe('Error Handling', () => {
+    test('long handleStr', () => {
+        const data = h.postRequest(h.USER_PROF_SET_HANDLE_URL, {
+          token: token0,
+          handleStr: "longhandleStr123456789",
+        });
+        expect(data).toStrictEqual({ error: expect.any(String) });
+      });
+      test('not alphanumeric handleStr', () => {
+        const data = h.postRequest(h.USER_PROF_SET_HANDLE_URL, {
+            token: token0,
+            handleStr: "h@ndleStr",
+        });
+        expect(data).toStrictEqual({ error: expect.any(String) });
+      });
+      test('handleStr already being used', () => {
+        const data = h.postRequest(h.USER_PROF_SET_HANDLE_URL, {
+          token: token0,
+          handleStr: handleStr2,
+        });
+        expect(data).toStrictEqual({ error: expect.any(String) });
+      });
+    
+
+  test('Invalid token', () => {
+    const data = h.postRequest(h.USER_PROF_SET_HANDLE_URL, {
+      token: h.invalidToken,
+      handleStr: 'firstnameLastname',
+    });
+    expect(data).toStrictEqual({ error: expect.any(String) });
+  });
+});
+
+// ------------------Function Testing------------------//
+
+describe('Function Testing', () => {
+  test('change handle', () => {
+    const data = h.postRequest(h.USER_PROF_SET_HANDLE_URL, {
+      token: token0,
+      handleStr: 'firstnameLastname',
+    });
+    expect(data).toStrictEqual({});
+  });
+});
+
