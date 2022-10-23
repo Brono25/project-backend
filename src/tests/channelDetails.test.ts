905f69c3b5253fb25c58f6e10da91ef00225f2c8
Shirlyn Alwis <z5417225@ad.unsw.edu.au>
2022-10-23 20:57:12 +1100

    converting to http

--- src/tests/channelDetails.test.ts
+++ src/tests/channelDetails.test.ts
@@ -13,35 +13,67 @@ import {
 let userId0: number;
 let userId1: number;
 let userId2: number;
-let token0: number;
-let token1: number;
-let token2: number;
+let token0: string;
+let token1: string;
+let token2: string;
 let channelId0: number;
 let channelId1: number;
 let channelId2: number;
 let invalidUserId: number;
-let invalidToken: number;
+let invalidToken: string;
 let invalidChannelId: number;
 beforeEach(() => {
-  // Users 0, 1, 2
-  let args: h.Args = [h.email0, h.password0, h.firstName0, h.lastName0];
-  userId0 = h.authRegisterReturnGaurd(authRegisterV1(...args));
-  token0 = authRegisterV1(...args).token;
-  args = [h.email1, h.password1, h.firstName1, h.lastName1];
-  userId1 = h.authRegisterReturnGaurd(authRegisterV1(...args));
-  token1 = authRegisterV1(...args).token;
-  args = [h.email2, h.password2, h.firstName2, h.lastName2];
-  userId2 = h.authRegisterReturnGaurd(authRegisterV1(...args));
-  token2 = authRegisterV1(...args).token;
-  // Channels 0,1,2
-  channelId0 = h.channelsCreateReturnGaurd(channelsCreateV1(userId0, h.channelName0, h.isPublic));
-  channelId1 = h.channelsCreateReturnGaurd(channelsCreateV1(userId0, h.channelName1, h.isPublic));
+  // Create users 0, 1, 2
+  const user0: any = h.postRequest(h.REGISTER_URL, {
+    email: h.email0,
+    password: h.password0,
+    nameFirst: h.firstName0,
+    nameLast: h.lastName0,
+  });
+  token0 = user0.token;
+  const user1: any = h.postRequest(h.REGISTER_URL, {
+    email: h.email1,
+    password: h.password1,
+    nameFirst: h.firstName1,
+    nameLast: h.lastName1,
+  });
+  token1 = user1.token;
+  const user2: any = h.postRequest(h.REGISTER_URL, {
+    email: h.email2,
+    password: h.password2,
+    nameFirst: h.firstName2,
+    nameLast: h.lastName2,
+  });
+  token1 = user2.token;
+  
+  // Create channels 0,1,2
+  const channel0: any = h.postRequest(h.CHAN_CREATE_URL, {
+    token: token0,
+    name: h.channelName0,
+    isPublic: h.isPublic,
+  });
+  channelId0 = channel0.channelId;
+  const channel1: any = h.postRequest(h.CHAN_CREATE_URL, {
+    token: token0,
+    name: h.channelName1,
+    isPublic: h.isPublic,
+  });
+  channelId1 = channel1.channelId;
+  const channel2: any = h.postRequest(h.CHAN_CREATE_URL, {
+    token: token0,
+    name: h.channelName2,
+    isPublic: h.isNotPublic,
+  });
+  channelId2 = channel2.channelId;
 
-  channelJoinV1(userId1, channelId1);
-  channelId2 = h.channelsCreateReturnGaurd(channelsCreateV1(userId0, h.channelName2, h.isNotPublic));
+  // User 1 joins Channel 1
+  h.postRequest(h.CHAN_JOIN_URL, {
+  token: token1,
+  channelId: channel1,
+  });
+
   // Error cases
-  invalidUserId = Math.abs(userId0) + Math.abs(userId1) + Math.abs(userId2) + 10;
-  invalidToken = Math.abs(token0) + Math.abs(token1) + Math.abs(token2) + 10;
+  invalidToken = h.invalidToken;
   invalidChannelId = Math.abs(channelId0) + Math.abs(channelId1) + Math.abs(channelId2) + 10;
 });
 // TEARDOWN
