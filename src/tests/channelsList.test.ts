494b8fd5f0290d9bd427ee57fabd9c388f84b583
Shirlyn Alwis <z5417225@ad.unsw.edu.au>
2022-10-27 09:24:27 +1100

    added tests for channelsListV2

--- src/tests/channelsList.test.ts
+++ src/tests/channelsList.test.ts
@@ -9,40 +9,83 @@ import { clearV1 } from '../other';
 import { channelInviteV1 } from '../channel';
 import * as h from './test.helper';
 
-let uId0: number;
-let uId1: number;
 let token0: string;
 let token1: string;
+let token2: string;
+let user: any;
+let uId0: number;
+let uId1: number;
+let uId2: number;
+let channelId0: number;
+let channelId1: number;
+let channelId2: number;
+let channel: any;
 let invalidToken: string;
-let publicChannelId0: number;
-let publicChannelId1: number;
-let privateChannelId0: number;
-let privateChannelId1: number;
-// Setup
+let invalidChannelId: number;
+// SETUP
 beforeEach(() => {
-  let args: h.Args = [h.email0, h.password0, h.firstName0, h.lastName0];
-  authUserId0 = h.authRegisterReturnGaurd(authRegisterV1(...args));
-  token0 = authRegisterV1(...args).token;
-  args = [h.email1, h.password1, h.firstName1, h.lastName1];
-  authUserId1 = h.authRegisterReturnGaurd(authRegisterV1(...args));
-  token1 = authRegisterV1(...args).token;
-
-  // User 1's owner of channels
-  args = [authUserId0, h.channelName0, h.isPublic];
-  publicChannelId0 = h.channelsCreateReturnGaurd(channelsCreateV1(...args));
-  args = [authUserId0, h.channelName1, h.isPublic];
-  publicChannelId1 = h.channelsCreateReturnGaurd(channelsCreateV1(...args));
-  args = [authUserId0, h.channelName2, h.isNotPublic];
-  privateChannelId0 = h.channelsCreateReturnGaurd(channelsCreateV1(...args));
-  args = [authUserId0, h.channelName3, h.isNotPublic];
-  privateChannelId1 = h.channelsCreateReturnGaurd(channelsCreateV1(...args));
-
-  // User 2 member of channels
-  channelInviteV1(authUserId0, privateChannelId0, authUserId1);
-  channelInviteV1(authUserId0, publicChannelId0, authUserId1);
-
-  invalidAuthUserId = Math.abs(authUserId0) + Math.abs(authUserId1) + 10;
+  // Create users 0, 1, 2
+  user = h.postRequest(h.REGISTER_URL, {
+    email: h.email0,
+    password: h.password0,
+    nameFirst: h.firstName0,
+    nameLast: h.lastName0,
+  });
+  token0 = user.token;
+  uId0 = parseInt(user.authUserId);
+  user1 = h.postRequest(h.REGISTER_URL, {
+    email: h.email1,
+    password: h.password1,
+    nameFirst: h.firstName1,
+    nameLast: h.lastName1,
+  });
+  token1 = user.token;
+  uId1 = parseInt(user.authUserId);
+  user = h.postRequest(h.REGISTER_URL, {
+    email: h.email2,
+    password: h.password2,
+    nameFirst: h.firstName2,
+    nameLast: h.lastName2,
+  });
+  token2 = user.token;
+  uId2 = parseInt(user.authUserId);
+  // Create channels 0,1,2
+  channel = h.postRequest(h.CHAN_CREATE_URL, {
+    token: token0,
+    name: h.channelName0,
+    isPublic: h.isPublic,
+  });
+  channelId0 = channel.channelId;
+  channel = h.postRequest(h.CHAN_CREATE_URL, {
+    token: token0,
+    name: h.channelName1,
+    isPublic: h.isNotPublic,
+  });
+  channelId1 = channel.channelId;
+  channel = h.postRequest(h.CHAN_CREATE_URL, {
+    token: token0,
+    name: h.channelName2,
+    isPublic: h.isPublic,
+  });
+  channelId2 = channel.channelId;
+  channel = h.postRequest(h.CHAN_CREATE_URL, {
+    token: token1,
+    name: h.channelName3,
+    isPublic: h.isNotPublic,
+  });
+  channelId3 = channel.channelId;
+  // User 1 joins Channel 0 and Channel 1
+  h.postRequest(h.CHAN_JOIN_URL, {
+  token: token1,
+  channelId: channelId0,
+  });
+  h.postRequest(h.CHAN_JOIN_URL, {
+    token: token1,
+    channelId: channelId2,
+  });
+  // Error cases
   invalidToken = h.invalidToken;
+  invalidChannelId = Math.abs(channelId0) + Math.abs(channelId1) + Math.abs(channelId2) + 10;
 });
 // Tear down
 afterEach(() => {
@@ -54,51 +97,68 @@ afterEach(() => {
 
 describe('Error Handling', () => {
   test('Invalid Token', () => {
-    expect(channelsListV1(invalidToken)).toStrictEqual({ error: expect.any(String) });
-  });
-
-  test('Invalid user ID', () => {
-    expect(channelsListV1(invalidAuthUserId)).toStrictEqual({ error: expect.any(String) });
+    const invalidInput: any = h.getRequest(h.CHAN_LIST_URL, {
+      token: invalidToken;
+    });
+    expect(invalidInput).toStrictEqual({ error: expect.any(String) });
   });
 });
 
 // ------------------Function Testing------------------//
 
 describe('Function Testing', () => {
-  test('List users channels with mix of public and private channels', () => {
-    expect(channelsListV1(token0)).toStrictEqual({
+  test('For Owner: List users channels with mix of public and private channels', () => {
+    const input: any = h.getRequest(h.CHAN_LIST_URL, {
+      token: token0,
+    });
+    expect(input).toStrictEqual({
       channels: <Channel[]>[
         {
-          channelId: publicChannelId0,
+          channelId: channelId0,
           name: h.channelName0,
         },
         {
-          channelId: publicChannelId1,
+          channelId: channelId1,
           name: h.channelName1,
         },
         {
-          channelId: privateChannelId0,
+          channelId: channelId2,
           name: h.channelName2,
         },
         {
-          channelId: privateChannelId1,
+          channelId: channelId3,
           name: h.channelName3,
         },
       ]
     });
   });
-  test('List channels when user has only private channels', () => {
-    expect(channelsListV1(token1)).toStrictEqual({
+  test('For Member: List channels with mix of public and private channels', () => {
+    const input: any = h.getRequest(h.CHAN_LIST_URL, {
+      token: token1,
+    });
+    expect(input).toStrictEqual({
       channels: <Channel[]>[
         {
-          channelId: publicChannelId0,
+          channelId: channelId0,
           name: h.channelName0,
         },
         {
-          channelId: privateChannelId0,
+          channelId: channelId2,
           name: h.channelName2,
         },
+        {
+          channelId: channelId3,
+          name: h.channelName3,
+        },
       ]
+    });
+  });
+  test('For user who has no channels', () => {
+    const input: any = h.getRequest(h.CHAN_LIST_URL, {
+      token: token2,
+    });
+    expect(input).toStrictEqual({
+      channels: <Channel[]>[]
     });
   });
 });
