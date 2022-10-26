f3599a2e7c1fbc1d021841ea8c66ba35e682251a
Shirlyn Alwis <z5417225@ad.unsw.edu.au>
2022-10-26 13:27:47 +1100

    adding tests

--- src/tests/channelAddOwner.test.ts
+++ src/tests/channelAddOwner.test.ts
@@ -1,2 +1,20 @@
 import * as h from './test.helper';
 
+// SETUP
+let user: any;
+let token0: string;
+let token1: string;
+let token2: string;
+let uId0: number;
+let uId1: number;
+let uId2: number;
+let channelId0: number;
+let channelId1: number;
+let channelId2: number;
+
+
+
+// member of 2 member channel
+// already owner
+// no owner permission, valid channelId
+// completely unrelated channel, with no common user
