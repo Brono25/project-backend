90cde53b73e735c6a661f78399e65b8b109ba1de
Brono25 <brono_25@hotmail.com>
2022-11-04 03:45:37 +1100

    coverage check

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,5 @@
-
-/** @type {import('jest').Config} */
-const config = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  maxWorkers: 1,
-};
-
-module.exports = config;
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    maxWorkers: 1,
+};  
