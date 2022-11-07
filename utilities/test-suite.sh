b974138899becfb68f6f4cfdb7261874a6baf323
Brono25 <brono_25@hotmail.com>
2022-11-08 00:42:53 +1100

    update test-suite

--- utilities/test-suite.sh
+++ utilities/test-suite.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 PORT=$(cat ./src/config.json| sed -n 3p|tr -dc '[0-9]')
+
 
 npm run lint-fix 
 npm run tsc
@@ -13,7 +14,7 @@ then
     PID=$(lsof -nP -i4TCP:$PORT | sed -E 's/[ ]+/ /g'|cut -d' '  -f2|tail -1)
 elif [[ "$OSTYPE" =~ ^linux ]]
 then
-    PID=$(netstat -an --tcp --program 2>/dev/null |grep "$PORT" | sed -E 's?.*LISTEN\s*([0-9]+)/node?\1?') 
+    PID=$(netstat -an --tcp --program 2>/dev/null |grep "$PORT" | sed -E 's?.*LISTEN\s*([0-9]+)/node.*?\1?') 
 fi
 
 if [[ "$PID" == '' ]] 
@@ -21,5 +22,11 @@ then
     echo "PID not found - server is not running"
     exit 1
 fi
+cat << eof
+
+    ⚡️ Killing PID: $PID belonging to PORT: $PORT ⚡️
+
+eof
+
 kill -INT $PID 
 npm run pipeline-coverage-check
