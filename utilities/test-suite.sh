65fa309a6e598b1ec95b1b5bd29532921b416bf9
Brono25 <brono_25@hotmail.com>
2022-11-08 18:54:59 +1100

    made function for finding pid

--- utilities/test-suite.sh
+++ utilities/test-suite.sh
@@ -4,6 +4,26 @@ trap 'kill -INT $PID 2> /dev/null; exit 1' INT
 
 PORT=$(cat ./src/config.json| sed -n 3p|tr -dc '[0-9]')
 
+
+function get_PID_of_PORT() {
+
+    if [[ "$OSTYPE" =~ ^darwin ]]
+    then 
+        PID=$(lsof -nP -i4TCP:$1 | sed -E 's/[ ]+/ /g'|cut -d' '  -f2|tail -1)
+    elif [[ "$OSTYPE" =~ ^linux ]]
+    then
+        PID=$(netstat -an --tcp --program 2>/dev/null |grep  ".*:$1\b.*LISTEN"|sed -E 's?.*LISTEN\s*([0-9]+)/node[ ]*$?\1?') 
+    fi 
+    [ "$PID" == '' ] && return 1
+    echo $PID 
+    return 0
+}
+
+# close a server it if is already running
+PID=$(get_PID_of_PORT $PORT) && kill -INT "$PID" 
+
+
+
 cat << eof
 
 |--------------------------------|
@@ -35,15 +55,11 @@ eof
 
 npm run ts-node-coverage 1> /dev/null &
 
-while [ "$PID" == '' ]; do
 
-    if [[ "$OSTYPE" =~ ^darwin ]]
-    then 
-        PID=$(lsof -nP -i4TCP:$PORT | sed -E 's/[ ]+/ /g'|cut -d' '  -f2|tail -1)
-    elif [[ "$OSTYPE" =~ ^linux ]]
-    then
-        PID=$(netstat -an --tcp --program 2>/dev/null |grep  ".*:$PORT\b.*LISTEN"|sed -E 's?.*LISTEN\s*([0-9]+)/node[ ]*$?\1?') 
-    fi 
+waiting=0
+while [ "$waiting" -ne 1 ]; do
+    PID=$(get_PID_of_PORT $PORT)
+    [ "$PID" == '' ] || waiting=1
 done
 
 npm t
@@ -52,7 +68,7 @@ npm t
 
 cat << eof
 
-⚡️ Killing PID: $PID belonging to PORT: $PORT ⚡️
+⚡️ Kill PID: $PID using PORT: $PORT ⚡️
 
 eof
 
