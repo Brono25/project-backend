d9e19cedc7c81af86c324baa96dfea35b71d5d00
Brono25 <brono_25@hotmail.com>
2022-11-16 14:40:40 +1100

    added helper

--- src/tests/userStats.test.ts
+++ src/tests/userStats.test.ts
@@ -25,7 +25,7 @@ afterEach(() => {
 
 describe('Error Handling', () => {
   test('Invalid token', () => {
-    h.testErrorThrown(h.USER_STATS, 'GET', 403, undefined, h.invalidToken);
+    h.testErrorThrown(h.USER_STATS_URL, 'GET', 403, undefined, h.invalidToken);
   });
 });
 
@@ -35,7 +35,7 @@ describe('Function Testing', () => {
   test('User just registered, initial stats', () => {
     const tmp: any = h.postRequest(h.REGISTER_URL, h.generateUserRegisterArgs(2));
     token2 = tmp.token;
-    const data: any = h.getRequest(h.USER_STATS, undefined, token2);
+    const data: any = h.getRequest(h.USER_STATS_URL, undefined, token2);
 
     expect(data).toStrictEqual({
       userStats: {
@@ -59,7 +59,7 @@ describe('Function Testing', () => {
     h.postRequest(h.CHAN_LEAVE_URL, { channelId: channelId2 }, token1);
     h.postRequest(h.CHAN_JOIN_URL, { channelId: channelId3 }, token1);
 
-    const data: any = h.getRequest(h.USER_STATS, undefined, token1);
+    const data: any = h.getRequest(h.USER_STATS_URL, undefined, token1);
     expect(data).toStrictEqual({
       userStats: {
         channelsJoined: [
@@ -79,7 +79,7 @@ describe('Function Testing', () => {
 
   test('Create 1 dm', () => {
     h.postRequest(h.DM_CREATE_URL, { uIds: [uId1] }, token0);
-    const data: any = h.getRequest(h.USER_STATS, undefined, token0);
+    const data: any = h.getRequest(h.USER_STATS_URL, undefined, token0);
     expect(data).toStrictEqual({
       userStats: {
         dmsJoined: [
@@ -95,7 +95,7 @@ describe('Function Testing', () => {
   test('Create 1 dm then leave dm', () => {
     const tmp: any = h.postRequest(h.DM_CREATE_URL, { uIds: [uId1] }, token0);
     h.postRequest(h.DM_LEAVE_URL, { dmId: tmp.dmId }, token0);
-    const data: any = h.getRequest(h.USER_STATS, undefined, token0);
+    const data: any = h.getRequest(h.USER_STATS_URL, undefined, token0);
     expect(data).toStrictEqual({
       userStats: {
         dmsJoined: [
@@ -112,7 +112,7 @@ describe('Function Testing', () => {
   test('Create 1 with 1 other memeber and then remove dm', () => {
     const tmp: any = h.postRequest(h.DM_CREATE_URL, { uIds: [uId1] }, token0);
     h.deleteRequest(h.DM_RMV_URL, { dmId: tmp.dmId }, token0);
-    let data: any = h.getRequest(h.USER_STATS, undefined, token0);
+    let data: any = h.getRequest(h.USER_STATS_URL, undefined, token0);
     expect(data).toStrictEqual({
       userStats: {
         dmsJoined: [
@@ -125,7 +125,7 @@ describe('Function Testing', () => {
         involvementRate: expect.any(Number)
       }
     });
-    data = h.getRequest(h.USER_STATS, undefined, token1);
+    data = h.getRequest(h.USER_STATS_URL, undefined, token1);
     expect(data).toStrictEqual({
       userStats: {
         dmsJoined: [
@@ -146,7 +146,7 @@ describe('Function Testing', () => {
     tmp = h.postRequest(h.CHAN_CREATE_URL, h.generateChannelsCreateArgs(0, true), token0);
     h.postRequest(h.MSG_SEND_URL, { channelId: tmp.channelId, message: h.message1 }, token0);
 
-    const data: any = h.getRequest(h.USER_STATS, undefined, token0);
+    const data: any = h.getRequest(h.USER_STATS_URL, undefined, token0);
     expect(data).toStrictEqual({
       userStats: {
         dmsJoined: [
