377fd6c20fcc242b1c5951a7ec7733a0ecb360de
Lamiya <z5356304@ad.unsw.edu.au>
2022-11-18 02:09:41 +1100

    added tests

--- src/tests/messageSendLaterDm.test.ts
+++ src/tests/messageSendLaterDm.test.ts
@@ -1,26 +1,43 @@
+import { NumericLiteral } from 'typescript';
 import * as h from './test.helper';
-import {
-  MessageId,
-} from '../data.types';
-
 h.deleteRequest(h.CLEAR_URL, {});
 
-// Setup
+// Setup: Create 3 users.
+let uId1: number;
 let token0: string;
 let token1: string;
-let channelId0: number;
-let invalidChannelId: number;
+let token2: string;
+let dmId0: number;
+let invalidDmId: number;
+let pastTime: number;
+let time: number;
 let tmp: any;
 beforeEach(() => {
-  h.deleteRequest(h.CLEAR_URL, {});
+  //tokens and uids
   tmp = h.postRequest(h.REGISTER_URL, h.generateUserRegisterArgs(0));
   token0 = tmp.token;
   tmp = h.postRequest(h.REGISTER_URL, h.generateUserRegisterArgs(1));
   token1 = tmp.token;
+  uId1 = parseInt(tmp.authUserId);
+  tmp = h.postRequest(h.REGISTER_URL, h.generateUserRegisterArgs(2));
+  token2 = tmp.token;
 
-  tmp = h.postRequest(h.CHAN_CREATE_URL, h.generateChannelsCreateArgs(0, true), token0);
-  channelId0 = parseInt(tmp.channelId);
-  invalidChannelId = Math.abs(channelId0) + 10;
+  //dms
+  tmp = h.postRequest(h.DM_CREATE_URL, { uIds: [uId1] }, token0);
+  dmId0 = parseInt(tmp.dmId);
+  invalidDmId = dmId0 + 10;
+
+  //send dm
+  tmp = h.postRequest(h.MSG_SEND_DM_URL, {
+    dmId: dmId0,
+    message: h.message0,
+  }, token0);
+
+  //get time
+  tmp = h.getRequest(h.USER_STATS_URL, undefined, token0);
+  pastTime= tmp.messagesSent[0].timeStamp;
+
+  time = pastTime + 10;
 });
 
 // Tear down
@@ -31,56 +48,81 @@ afterEach(() => {
 // ------------------Error Testing------------------//
 
 describe('Error Handling', () => {
-  test('Invalid channel ID', () => {
+
+  test('Invalid DmId', () => {
     const data = {
-      channelId: invalidChannelId,
-      message: h.message0
+      dmId: invalidDmId,
+      message: h.message0,
+      timeSent: time,
     };
-    h.testErrorThrown(h.MSG_SEND_URL, 'POST', 400, data, token0);
+    h.testErrorThrown(h.MSG_SEND_LATER_DM_URL, 'POST', 400, data, token0);
   });
-  test('Message too short', () => {
+
+  test('Message less than length 1', () => {
     const data = {
-      channelId: channelId0,
+      dmId: dmId0,
       message: h.invalidShortMessage,
+      timeSent: time,
     };
-    h.testErrorThrown(h.MSG_SEND_URL, 'POST', 400, data, token0);
+    h.testErrorThrown(h.MSG_SEND_LATER_DM_URL, 'POST', 400, data, token0);
   });
-  test('Message too long', () => {
+
+  test('Message more than length 1000', () => {
     const data = {
-      channelId: channelId0,
+      dmId: dmId0,
       message: h.invalidLongMessage,
+      timeSent: time,
     };
-    h.testErrorThrown(h.MSG_SEND_URL, 'POST', 400, data, token0);
+    h.testErrorThrown(h.MSG_SEND_LATER_DM_URL, 'POST', 400, data, token0);
   });
-  test('Valid channel, user exists but is not a member', () => {
+
+  test('timeSent is a time in the past', () => {
     const data = {
-      channelId: channelId0,
-      message: h.message0
+      dmId: dmId0,
+      message: h.invalidLongMessage,
+      timeSent: pastTime,
     };
-    h.testErrorThrown(h.MSG_SEND_URL, 'POST', 403, data, token1);
+    h.testErrorThrown(h.MSG_SEND_LATER_DM_URL, 'POST', 400, data, token0);
   });
-  test('Invalid Token', () => {
+
+  test('User is not a member', () => {
     const data = {
-      channelId: channelId0,
-      message: h.message0
+      dmId: dmId0,
+      message: h.message0,
+      timeSent: time,
     };
-    h.testErrorThrown(h.MSG_SEND_URL, 'POST', 403, data, h.invalidToken);
+    h.testErrorThrown(h.MSG_SEND_LATER_DM_URL, 'POST', 403, data, token2);
   });
+
+  test('Invalid token', () => {
+    const data = {
+      dmId: dmId0,
+      message: h.message0,
+      timeSent: time,
+    };
+    h.testErrorThrown(h.MSG_SEND_LATER_DM_URL, 'POST', 403, data, h.invalidToken);
+  });
+
 });
 
 // ------------------Function Testing------------------//
 
 describe('Function Testing', () => {
-  test('Sending a message', () => {
-    let data = h.postRequest(h.MSG_SEND_URL, {
-      channelId: channelId0,
-      message: h.message0
+  test('Owner Send  DM message', () => {
+    const data = h.postRequest(h.MSG_SEND_LATER_DM_URL, {
+      dmId: dmId0,
+      message: h.message0,
+      timeSent: time,
     }, token0);
-    expect(data).toStrictEqual(<MessageId>{ messageId: expect.any(Number) });
-    data = h.postRequest(h.MSG_SEND_URL, {
-      channelId: channelId0,
-      message: h.message0
-    }, token0);
-    expect(data).toStrictEqual(<MessageId>{ messageId: expect.any(Number) });
+    expect(data).toStrictEqual({ messageId: expect.any(Number) });
+  });
+  
+  test('Member Send DM message', () => {
+    const data = h.postRequest(h.MSG_SEND_LATER_DM_URL, {
+      dmId: dmId0,
+      message: h.message0,
+      timeSent: time,
+    }, token1);
+    expect(data).toStrictEqual({ messageId: expect.any(Number) });
   });
 });
